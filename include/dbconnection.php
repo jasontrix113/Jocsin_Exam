777dbf6dc460466e0c8d9c21006bd6dfbbb47e43
Jason Jocsin <jsnjocsin@gmail.com>
2018-02-07 21:15:03 -0800

    Final AddBook

---
+++ include/dbconnection.php
@@ -0,0 +1,10 @@
+<?php
+	
+	$host ="localhost";
+	$user = "root";
+	$pass = "";
+	$db = "library_db";
+
+	$conn = mysqli_connect($host, $user, $pass, $db)or die('Cannot connect to database.');
+
+?>
